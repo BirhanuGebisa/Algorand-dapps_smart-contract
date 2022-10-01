6cc70e469ec35ae1d13a1d622123ea007a755f62
BirhanuGebisa <birhanugebisa@gmail.com>
2022-10-02 00:48:12 +0300

    backend: view account  file update

--- webapps/views_pages.py
+++ webapps/views_pages.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, logout_user, current_user
 
 from .forms import SendForm, AssetForm, FilterForm
-
+#update view all pages
 main_bp = Blueprint(
     'main_bp', __name__,
     template_folder='frontend_page',
